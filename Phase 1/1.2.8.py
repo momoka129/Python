5cf4acea296352c2df8d4d11e9b2ff26490efe07
Kinoko <huiyuanai129@outlook.com>
2024-09-20 18:18:22 +0800

    three ways to define a sting variable

---
+++ Phase 1/1.2.8.py
@@ -0,0 +1,16 @@
+name_1 = 'Lily'
+name_2 = "Morse"
+name_3 = """
+Pepper
+Rick
+"""
+
+print(name_1, name_2, name_3)
+
+# include " " inside a string variable
+name_4 = '"vv"'
+print(name_4)
+
+# or use \
+name_5 = "\"name5"
+print(name_5)
