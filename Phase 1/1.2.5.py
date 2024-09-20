03a4d6e2a82f322cd0a803d4868458e7a704e2a6
Kinoko <huiyuanai129@outlook.com>
2024-09-20 17:52:50 +0800

    converting data type

---
+++ Phase 1/1.2.5.py
@@ -0,0 +1,12 @@
+# converting int to string
+res = str(11)
+print("The data type of 11 rn becomes", type(res))
+
+# converting float to int
+res1 = int(1.29)
+print(type(res1), res1)
+
+#converting int to float
+res2 = float(1)
+print(type(res2), res2)
+
