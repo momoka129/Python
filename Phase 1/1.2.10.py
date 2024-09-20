eed8167ef0a4794e27bf644f80ea0afb3171648f
Kinoko <huiyuanai129@outlook.com>
2024-09-20 18:32:01 +0800

    String formatting

---
+++ Phase 1/1.2.10.py
@@ -0,0 +1,12 @@
+game = "Brawl Stars"
+message1 = "I want to play %s" %game
+print(message1)
+
+time_game = 3
+num = 1
+message2 = "I have to play %s accounts every %s day." %(time_game, num)
+print(message2)
+
+# %s 将内容转化为字符串 放入占位位置
+# %d 将内容转化为整数 放入占位位置 digit
+# %f 将内容转化为浮点型 放入占位位置
