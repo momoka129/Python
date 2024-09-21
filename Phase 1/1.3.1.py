3725eaf77a473c2d65a52bc7f94c532885b92793
Kinoko <huiyuanai129@outlook.com>
2024-09-21 16:25:28 +0800

    bool, compare operators

---
+++ Phase 1/1.3.1.py
@@ -0,0 +1,11 @@
+bool_1 = True
+bool_2 = False
+
+print("10 > 5 result: %s" %(10 > 5)) 
+
+# compare operators
+# == , != , > , < , >= , <=
+
+num1 = 1
+num2 = 3
+print(f"num1 is the same with num2: {num1 == num2}")
