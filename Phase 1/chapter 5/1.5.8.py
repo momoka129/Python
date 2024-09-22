274b80e35b748b1fc5c13f48b1dc143eb864e2c8
Kinoko <huiyuanai129@outlook.com>
2024-09-22 16:04:28 +0800

    function comments

---
+++ Phase 1/chapter 5/1.5.8.py
@@ -0,0 +1,10 @@
+def add(x, y): 
+    """
+    add function...
+    :param x: ....
+    :param y: ....
+    :return:  description
+    """
+    result = x + y
+    print(f"{x},{y} summation: {result}")
+    return result
