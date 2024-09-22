76ad4dc3e465f419b7f4942174d980cb29f2bbc3
Kinoko <huiyuanai129@outlook.com>
2024-09-22 11:46:39 +0800

    www

--- Phase 1/1.4.10.py
+++ Phase 1/1.4.10.py
@@ -1,8 +1,11 @@
-for i in range(100):
+for i in range(8):
     print(f"Show love to Yanami on DAY {i+1}")
 
     for j in range(3):
         print("I like you!")
         j += 1
     
+    print()
     i += 1
+
+print("Begin a relationship!")
