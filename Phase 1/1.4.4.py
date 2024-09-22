542db18aa59f9876deb27325f3494f053040a009
Kinoko <huiyuanai129@outlook.com>
2024-09-22 11:05:28 +0800

    while nesting

---
+++ Phase 1/1.4.4.py
@@ -0,0 +1,15 @@
+# outer loop
+i = 1
+while i <= 100:
+    print(f"today is {i}th day. Ready to confess")
+    
+    # inner loop
+    j = 1
+    while j <= 10:
+        print(f"Give away {j}th rose.")
+        j += 1
+    
+    print("Yanami san, i like you!")
+    i+=1
+    
+print("I've found my true love.")
