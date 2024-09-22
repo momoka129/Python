523b828db11c520b5ef18fd22ef31cca792877a3
Kinoko <huiyuanai129@outlook.com>
2024-09-22 11:37:07 +0800

    range statement

---
+++ Phase 1/1.4.8.py
@@ -0,0 +1,13 @@
+# range(num)
+# range(5)  [0,1,2,3,4]
+
+# range(num1, num2)
+
+# range(num1, num2, step)
+
+for x in range(2, 10, 2):
+    print(x)
+
+# use range to set the for loop execution times
+for x in range(10):
+    print(f"This execute {x} times.")
