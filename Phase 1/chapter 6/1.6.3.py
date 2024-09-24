0228401f0ecd10f0955eadee480e0ff3316b952e
Kinoko <huiyuanai129@outlook.com>
2024-09-24 11:14:35 +0800

    list index: inverted index.., nested index

---
+++ Phase 1/chapter 6/1.6.3.py
@@ -0,0 +1,9 @@
+# list index
+name_list = ['Yanami', 'Kinoko']
+print(name_list[0], name_list[1])
+
+# inverted index
+print(name_list[-1], name_list[-2])
+
+my_list = [[0, 1], [2, 3]]
+print(my_list[0][-1])
