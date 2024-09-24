9385e563c191088fe04414f43bbf27c0af7e6d9c
Kinoko <huiyuanai129@outlook.com>
2024-09-24 11:09:15 +0800

    list definition

---
+++ Phase 1/chapter 6/1.6.2.py
@@ -0,0 +1,12 @@
+# define empty list
+list_1 = []
+list_2 = list()
+
+# list can store data that has different data type!! 
+my_list = ['Losing heroine', 11, True]
+print(my_list)
+print(type(my_list))
+
+# support nesting
+list_3 = [[1,2,3],['H',False]]
+print(list_3)
