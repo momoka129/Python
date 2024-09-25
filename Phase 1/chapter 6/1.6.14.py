c537751ee31f7b3b45e8238dc097cdd60fd0bf1d
Kinoko <huiyuanai129@outlook.com>
2024-09-25 20:52:51 +0800

    dictionary definition

---
+++ Phase 1/chapter 6/1.6.14.py
@@ -0,0 +1,14 @@
+my_dit = {"Yanami": 52, 52: "My wife"}
+
+# define empty dictionary
+dit_em = {}
+dit = dict()
+
+print(my_dit)
+
+# key cannot repeat
+
+# 不能使用下标索引 - 通过key检索value
+
+# 键值对可以是任意类型 - can dictionary nesting
+print(my_dit[my_dit["Yanami"]])
