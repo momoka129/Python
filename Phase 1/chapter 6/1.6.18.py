2c7c14864b176593fc94357f3c1035f3a22c865f
Kinoko <huiyuanai129@outlook.com>
2024-09-26 10:46:55 +0800

    max, min, sorted( )

---
+++ Phase 1/chapter 6/1.6.18.py
@@ -0,0 +1,13 @@
+my_str = "123456"
+
+print(max(my_str))
+
+# 容器转列表
+# 容器转元组
+# 容器转字符串
+# 容器转集合
+
+my_list = [1,3,56,3,5,6,7,34]
+print(sorted(my_list))
+print(sorted(my_list, reverse=True))
+print(my_list)  # original list not changed
