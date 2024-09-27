83d7a199e342e87d6295bc554e65e992916098fd
Kinoko <huiyuanai129@outlook.com>
2024-09-27 16:07:37 +0800

    python module import ways

---
+++ Phase 1/chapter 9/1.9.4.py
@@ -0,0 +1,21 @@
+
+# # 1 import time module
+# import time
+
+# print("Nap")
+# time.sleep(2)
+# print("wake up")
+
+# 2
+from time import sleep
+
+print("Nice to meet you!")
+sleep(1)
+print("Nice to meet you too!")
+
+# 3 
+# from time import *
+
+# 4 
+# import time as t (alias)
+
