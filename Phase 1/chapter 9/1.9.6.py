4943afd2d9d89505ec54cf5f76a338c61026fabf
Kinoko <huiyuanai129@outlook.com>
2024-09-27 16:46:12 +0800

    custom python package

---
+++ Phase 1/chapter 9/1.9.6.py
@@ -0,0 +1,6 @@
+import my_package
+import my_package.module1
+import my_package.module2
+
+my_package.module1.print_A()
+my_package.module2.print_B()
---
+++ Phase 1/chapter 9/my_package/module1.py
@@ -0,0 +1,3 @@
+
+def print_A():
+    print("A")
