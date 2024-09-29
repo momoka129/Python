6dc87699a7077fc17e8c5112ba9f4a475b3b7e6f
Kinoko <huiyuanai129@outlook.com>
2024-09-29 14:13:51 +0800

    learn class

---
+++ Phase 2/chapter 1/2.1.1.py
@@ -0,0 +1,18 @@
+# design a class
+class Student:
+    name = None
+    gender = None
+    nationality = None
+    age = None
+
+# create a object
+stu_1 = Student()
+
+# assign value for the object
+stu_1.name = "Alice"
+stu_1.gender = "Female"
+stu_1.nationality = "Spanish"
+stu_1.age = 22
+
+print(stu_1)
+print(stu_1.name)
