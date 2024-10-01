867ffcad555c915e18a72b9424f2c475c052b925
Kinoko <huiyuanai129@outlook.com>
2024-10-01 11:52:01 +0800

    type annotation

---
+++ Phase 2/chapter 1/2.1.10.py
@@ -0,0 +1,20 @@
+import random
+
+#
+v1: int = 10
+v2: float = 3.41
+
+# 
+class Student:
+    pass
+stu: Student = Student()
+
+# 
+my_list: list = [2,4,5]
+
+#
+my_list: list[int] = [2,4,5]
+my_dict: dict[int, str] = {1: "OI"}
+
+#
+var_1 = random.randint(1, 10)   # type: int
