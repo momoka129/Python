90ecd3658df57e298210df0dcdfce23e36256f6b
Kinoko <huiyuanai129@outlook.com>
2024-10-01 11:26:50 +0800

    inheritance

--- Phase 2/chapter 1/2.1.8.py
+++ Phase 2/chapter 1/2.1.8.py
@@ -1,1 +1,31 @@
-       
+# single inheritance
+class Phone:
+    IMEI = None
+    producer = "HM"
+
+    def call_4G(self):
+        print("4G calling")
+
+class Phone2022(Phone):
+    face_id = "48935"
+
+    def call_5G(self):
+        print("5g calling")
+
+p = Phone2022()
+print(p.producer)
+p.call_4G()
+p.call_5G()
+
+
+# multiple inheritance
+class NFCReader:
+    nfc_type = "nfc-5"
+
+# if parents classes have attributes with the same name, priority is given from left to right 
+class Mi_Phone(Phone, NFCReader):
+    producer = "Mi"
+
+mp = Mi_Phone()
+print(mp.producer)
+print(mp.nfc_type)
